b3361feb056a0c647a53b6072a8d5c28ab9ee971
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2018-10-07 13:00:07 +0200

    Added Splash Screen

---
+++ BubblePokeAndroid/app/src/main/java/io/github/a0x48piraj/bubble_poke/SplashActivity.java
@@ -0,0 +1,32 @@
+package io.github.a0x48piraj.bubble_poke;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        intent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
